bce1ab11bd27d3ed2945413bb06b3c0e58023f7c
Martin Secany <mrtnxx@gmail.com>
2021-11-02 16:28:05 +0100

    store page

--- HrajMi/js/main.js
+++ HrajMi/js/main.js
@@ -152,14 +152,16 @@ jQuery(document).ready(function($) {
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 500,
-      values: [ 75, 300 ],
+      max: 1000,
+		get values(){
+		  return [this.min, this.max]
+		},
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+			$( "#amount" ).val( ui.values[ 0 ] + " € - " + ui.values[ 1 ] + " €");
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ) +
+      " € - " + $( "#slider-range" ).slider( "values", 1 ) + " €" );
 	};
 	siteSliderRange();
 
